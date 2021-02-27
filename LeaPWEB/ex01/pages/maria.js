b4599705c2c1ef013cf220ade89a50812529e170
Zaujulio <zauhdf@gmail.com>
2021-02-27 03:25:32 -0300

    add nextjs link Maria Preá

---
+++ LeaPWEB/ex01/pages/maria.js
@@ -0,0 +1,10 @@
+import styles from "../styles/pages/Home.module.scss";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <a></a>
+    </div>
+  );
+}
