51d8af5f85df92d2136e6d034d260a145135df0c
Zaujulio <zauhdf@gmail.com>
2021-02-27 03:40:09 -0300

    separete components

---
+++ LeaPWEB/ex01/components/Footer.js
@@ -0,0 +1,12 @@
+import styles from '../styles/components/Footer.module.scss';
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <p>For other projects and more information</p>
+      <a href="https://github.com/ZauJulio" target="_blank">
+        <img src="https://github.com/zaujulio.png" alt="Zaú Júlio"></img>
+      </a>
+    </footer>
+  );
+}
