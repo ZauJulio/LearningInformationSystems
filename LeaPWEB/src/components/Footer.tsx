778a8bf611753f3d1cf60734c853ec0b45ffd2c0
Zaujulio <zauhdf@gmail.com>
2021-03-07 04:35:27 -0300

    footer sintaxe correction

--- LeaPWEB/src/components/Footer.tsx
+++ LeaPWEB/src/components/Footer.tsx
@@ -2,7 +2,7 @@ import styles from '../styles/components/Footer.module.scss';
 
 export function Footer() {
   return (
-    <footer className={styles.footer}>
+    <footer className={styles.footerContainer}>
       <p>For other projects and more information</p>
       <a href="https://github.com/ZauJulio" target="_blank">
         <img src="https://github.com/zaujulio.png" alt="Zaú Júlio"></img>
